7f0f412953959936d2d94a30c6523f80294f75a7
Nick Matthews <nick@kizzyco.com>
2013-01-10 19:11:49 +0000

    Redesign the Researchers List page.
    git-svn-id: https://svn.code.sf.net/p/thefamilypack/code/tfp/trunk@380 0b5b8454-fdc9-41d1-a9ee-242ce37e0c65

--- src/tfpWrAdmin.cpp
+++ src/tfpWrAdmin.cpp
@@ -50,16 +50,70 @@ wxString tfpWriteResearcherList()
     recResearcherVec list = recResearcher::GetResearchers();
     idt curUserID = recGetCurrentUser();
 
-    htm << "<html><head><title>Researchers</title>"
-           "<meta http-equiv='Content-Type' content='text/html;charset=UTF-8'>"
-           "<link rel='stylesheet' type='text/css' href='memory:tfp______.css'>"
-           "</head><body>"
-           "<h1>Researchers</h1><table>";
+    htm << 
+        "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\""
+        "\"http://www.w3.org/TR/html4/loose.dtd\">\n"
+        "<html>\n<head>\n"
+        "<title>Researchers</title>\n"
+        "<meta http-equiv='Content-Type' content='text/html;charset=utf-8'>\n"
+        "<link rel='stylesheet' type='text/css' href='memory:tfp.css'>\n"
+        "</head>\n<body>\n<div class='tfp'>\n"
+        "<h1>Researchers</h1>\n"
+    ;
+
     for( size_t i = 0 ; i < list.size() ; i++ ) {
+        size_t note = ( list[i].FGetComments().size() ) ? 1 : 0;
         recContactList cl( list[i].FGetConListID() );
         idt userID = list[i].GetUserID();
+        recContactVec contacts = list[i].GetContacts();
+        size_t csize = contacts.size() + note;
 
-        htm << "<tr><td>";
+        htm << 
+            "<table class='data'>\n"
+            "<tr><th>Name</th><th colspan='3'>Details</th></tr>\n"
+
+            "<tr>\n<td rowspan='" << csize << "'>" << list[i].FGetName() <<
+            "</td>\n"
+        ;
+        if( note ) {
+            htm << 
+                "<td colspan='3'>" << list[i].FGetComments() <<
+                "</td>\n</tr>\n"
+            ;
+        }
+        if( csize ) {
+            for( size_t j = 0 ; j < contacts.size() ; j++ ) {
+                if( j > 0 || note == 1 ) {
+                    htm << "<tr>\n";
+                }
+                htm <<
+                    "<td><b><a href='tfpe:C" << contacts[j].FGetID() <<
+                    "'>" << contacts[j].GetIdStr() <<
+                    "</a></b></td>\n<td>" << contacts[j].GetTypeStr() <<
+                    "</td>\n<td>" << contacts[j].GetHtmlValue() <<
+                    "</td>\n</tr>\n"
+                ;
+            }
+        } else {
+            htm << "<td colspan='3'></td>\n</tr>\n";
+        }
+        htm <<
+            "<tr>\n<td colspan='4' class='status'>\n"
+            "<b><a href='tfpe:Re" << list[i].FGetID() <<
+            "'>" << list[i].GetIdStr() << 
+            "</a></b>"
+        ;
+        if( userID ) {
+            htm << 
+                ", <b>" << recUser::GetIdStr( userID ) <<
+                "</b>"
+            ;
+            if( userID == curUserID ) {
+                htm << " [Current User]";
+            }
+        }
+        htm << "</td>\n</tr>\n</table>\n";
+#if 0
         if( cl.FGetIndID() ) {
             htm << "<a href='tfp:FI" << cl.FGetIndID()
                 << "'>" << list[i].FGetName() << "</a>";
@@ -76,6 +130,7 @@ wxString tfpWriteResearcherList()
                 htm << "*";
             }
         }
+
         htm << "</td><td><a href='tfpe:Re" << list[i].FGetID()
             << "'><img src=memory:edit.png></a></td><td>" << list[i].FGetComments()
             << "</td></tr>";
@@ -86,14 +141,15 @@ wxString tfpWriteResearcherList()
 
             for( size_t j = 0 ; j < contacts.size() ; j++ ) {
                 htm << "<tr><td valign=top align=right>" << contacts[j].GetTypeStr()
-                    << ":</td><td>" << contacts[j].GetHtmlValue( "!" )
+                    << ":</td><td>" << contacts[j].GetHtmlValue()
                     << "</td></tr>";
             }
             htm << "</table></td></tr>";
         }
+#endif
     }
 
-    htm << "</table></body></html>";
+    htm << "</div></body></html>";
 
     return htm;
 }
