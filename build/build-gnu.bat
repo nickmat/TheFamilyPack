339ee1686d542e2bb30fc35d511ca6d7c1b7092c
Nick Matthews <nick@kizzyco.com>
2013-02-17 18:11:24 +0000

    gnu build doesn't require file2cpp utility.
    git-svn-id: https://svn.code.sf.net/p/thefamilypack/code/tfp/trunk@401 0b5b8454-fdc9-41d1-a9ee-242ce37e0c65

--- build/build-gnu.bat
+++ build/build-gnu.bat
@@ -1,4 +1,4 @@
-bakefile -f gnu -I %WXWIN%\build\bakefiles\wxpresets -D BUILD_DIR=gnu -o gnu/makefile tfp.bkl -DNO_UTILS=1
+bakefile -f gnu -I %WXWIN%\build\bakefiles\wxpresets -D BUILD_DIR=gnu -o gnu/makefile tfp.bkl -DNO_UTILS=1 -DNO_DEPS
 
 @echo off
 if '%1' == 'cont' goto end
