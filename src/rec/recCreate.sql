ac2bcc7e0e46624edab1bef15b6d5107aee8ef69
Nick Matthews <nick@kizzyco.com>
2013-04-03 17:55:02 +0000

    Update recCreate.sql to new version 0.0.10.0.
    git-svn-id: https://svn.code.sf.net/p/thefamilypack/code/tfp/trunk@471 0b5b8454-fdc9-41d1-a9ee-242ce37e0c65

--- src/rec/recCreate.sql
+++ src/rec/recCreate.sql
@@ -453,7 +453,7 @@ INSERT INTO UserSetting (id, user_id, property, val) VALUES(1, 0, 1, 'F1');
 INSERT INTO UserSetting (id, user_id, property, val) VALUES(2, 1, 1, 'F1');
 
 /* The Version table has only this one row */
-INSERT INTO Version (id, major, minor, revision, test) VALUES(1, 0, 0, 9, 26);
+INSERT INTO Version (id, major, minor, revision, test) VALUES(1, 0, 0, 10, 0);
 
 COMMIT;
 
