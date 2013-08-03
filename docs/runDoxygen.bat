0c4871e4507e46f227ea710fe34d9dcfb1826f0f
Nick Matthews <nick@kizzyco.com>
2013-08-03 11:40:33 +0000

    Belatedly, update doxygen files. These now only document (some of) the public interface of the included libraries.
    git-svn-id: https://svn.code.sf.net/p/thefamilypack/code/tfp/trunk@521 0b5b8454-fdc9-41d1-a9ee-242ce37e0c65

--- docs/runDoxygen.bat
+++ docs/runDoxygen.bat
@@ -1,6 +1,11 @@
 cd doxygen
+if exist tfp rmdir /S /Q tfp
 doxygen tfp.doxy
+if exist cal rmdir /S /Q cal
 doxygen cal.doxy
+if exist rec rmdir /S /Q rec
 doxygen rec.doxy
+if exist recgui rmdir /S /Q recgui
+doxygen recgui.doxy
 
 pause
