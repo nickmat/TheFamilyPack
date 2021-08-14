4ce4be98013e445a0606db9617e8d20ccd952972
Nick Matthews <nick@kizzyco.com>
2021-08-14 11:05:47 +0100

    TFP: Add src/rg/rgRefTemplate.h aand .cpp files.
    Update build system.

---
+++ src/rg/rgRefTemplate.h
@@ -0,0 +1,33 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Name:        src/rg/rgRefTemplate.h
+ * Project:     The Family Pack: Genealogy data storage and display program.
+ * Purpose:     Use template to input reference data dialog header.
+ * Author:      Nick Matthews
+ * Website:     http://thefamilypack.org
+ * Created:     13 Aug 2021
+ * Copyright:   Copyright (c) 2021, Nick Matthews.
+ * Licence:     GNU GPLv3
+ *
+ *  The Family Pack is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The Family Pack is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with The Family Pack.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+*/
+
+#ifndef SRC_RG_RGREFTEMPLATE_H
+#define SRC_RG_RGREFTEMPLATE_H
+
+
+
+#endif // SRC_RG_RGREFTEMPLATE_H
