7b4ca4e60b0d8dc1e448a7279b171660bae91680
Nick Matthews <nick@kizzyco.com>
2022-02-19 04:21:36 +0000

    rec Library bug fix. recEventa::UpdateDatePoint function always set to date1.
    Add return statements after setting f_date_pt inside case statement.

--- src/rec/recEventa.cpp
+++ src/rec/recEventa.cpp
@@ -391,9 +391,11 @@ void recEventa::UpdateDatePoint( const wxString& dbname )
     case recEventTypeGrp::birth:
     case recEventTypeGrp::nr_birth:
         f_date_pt = recDate::GetDatePoint( f_date1_id, recDate::DatePoint::beg );
+        return;
     case recEventTypeGrp::death:
     case recEventTypeGrp::nr_death:
         f_date_pt = recDate::GetDatePoint( f_date1_id, recDate::DatePoint::end );
+        return;
     }
     f_date_pt = recDate::GetDatePoint( f_date1_id );
 }
