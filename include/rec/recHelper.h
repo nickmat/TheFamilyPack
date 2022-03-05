019be4da51b9a44b4f8917189f901db7ea79cbdc
Nick Matthews <nick@kizzyco.com>
2022-03-05 14:36:29 +0000

    rec Library, add helper function recIsCharNumber,
    Checks a char in a string to see if it is part of a number, ie a digit or hyphen.

--- include/rec/recHelper.h
+++ include/rec/recHelper.h
@@ -106,6 +106,10 @@ extern recSplitStrRet recSplitStr( const wxString& str, idt* id1, idt* id2, wxSt
 enum class Sex { unstated, male, female, unknown };
 extern wxString recGetSexStr( Sex sex );
 
+inline bool recIsCharNumber( const wxString& str, size_t pos ) {
+    return str.size() > pos && (wxIsdigit( str.GetChar( pos ) ) || str.GetChar( pos ) == '-');
+}
+
 // Surname grouping.
 enum recSurnameGroup {
     recSG_DefaultInd, recSG_Individual, recSG_Persona, recSG_All, recSG_MAX
