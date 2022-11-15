3f039e844e2a42278120ba5bb37af27af7050abe
Nick Matthews <nick@kizzyco.com>
2022-11-15 12:13:25 +0000

    rec Library, complete recExportCsv function.

--- src/rec/recCsv.cpp
+++ src/rec/recCsv.cpp
@@ -37,27 +37,52 @@
 
 #include <wx/dir.h>
 
-#include <fstream>
-#include <iostream>
-#include <string>
 
 using std::string;
 
-template <class T>
-bool WriteTableCsv( const string& csv_dir )
-{
-    string fname = csv_dir + T::TableName() + ".csv";
-    std::ofstream ofile( fname, std::ios::trunc );
-    if( !ofile ) {
-        return false;
+namespace {
+
+    bool csvWriteMediaData( const string& csv_dir ) {
+        string md_dir, image_dir, md_fname, image_fn;
+        recIdVec assIDs = recAssociate::IdVec( recDb::Coverage::all );
+        for( idt assID : assIDs ) {
+            if( assID == 0 ) {
+                md_dir = csv_dir;
+            }
+            else {
+                md_dir = csv_dir + "ass" + recGetStr( assID ) + recFileSep();
+                wxDir::Make( md_dir );
+            }
+            wxString assdb = recAssociate::GetAttachedName( assID, "Main" );
+
+            recDb::Coverage cover = recDb::Coverage::rnotzero;
+            if( recMediaData::Count( cover, assdb ) == 0 ) {
+                continue;
+            }
+            bool ret = recMediaData::CsvWriteTableFile( md_dir, cover, assdb );
+            if( !ret ) {
+                continue;
+            }
+            if( assID != 0 ) {
+                string create_fn = md_dir + "create.sql";
+                recDb::WriteCreateScript( create_fn, recDb::DbType::media_data_only );
+            }
+
+            image_dir = md_dir + "image" + recFileSep();
+            wxDir::Make( image_dir );
+
+            recIdVec list = recMediaData::IdVec( recDb::Coverage::rnotzero, assdb );
+            for( idt mdID : list ) {
+                recMediaData md( mdID, assID );
+                image_fn = image_dir + "md" + recGetStr( mdID ) + ".jpg";
+                md.ExportData( image_fn );
+            }
+        }
+        return true;
     }
-    recIdVec list = T::AllIDs();
-    ofile << T::CsvTitles();
-    for( idt id : list ) {
-        T::CsvWrite( ofile, id );
-    }
-    return true;
-}
+
+} // namespace
+
 
 bool recExportCsv( const string& path )
 {
@@ -66,7 +91,52 @@ bool recExportCsv( const string& path )
         return false;
     }
     string pathsep = dir.GetNameWithSep();
-    bool ret = WriteTableCsv<recAssociate>( pathsep );
-    // TODO: 
+    recDb::WriteCreateScript( pathsep + "create.sql", "Main" );
+
+    bool ret = csvWriteMediaData( pathsep );
+    ret = ret && recAssociate::CsvWriteTableFile( path );
+    ret = ret && recCitation::CsvWriteTableFile( path );
+    ret = ret && recRepository::CsvWriteTableFile( path );
+    ret = ret && recCitationPart::CsvWriteTableFile( path );
+    ret = ret && recCitationPartType::CsvWriteTableFile( path );
+    ret = ret && recContactList::CsvWriteTableFile( path );
+    ret = ret && recContactType::CsvWriteTableFile( path );
+    ret = ret && recContact::CsvWriteTableFile( path );
+    ret = ret && recDate::CsvWriteTableFile( path );
+    ret = ret && recRelativeDate::CsvWriteTableFile( path );
+    ret = ret && recEvent::CsvWriteTableFile( path );
+    ret = ret && recEventa::CsvWriteTableFile( path );
+    ret = ret && recEventaPersona::CsvWriteTableFile( path );
+    ret = ret && recEventEventa::CsvWriteTableFile( path );
+    ret = ret && recEventType::CsvWriteTableFile( path );
+    ret = ret && recEventTypeRole::CsvWriteTableFile( path );
+    ret = ret && recFamilyEvent::CsvWriteTableFile( path );
+    ret = ret && recFamilyEventa::CsvWriteTableFile( path );
+    ret = ret && recFamily::CsvWriteTableFile( path );
+    ret = ret && recFamilyIndividual::CsvWriteTableFile( path );
+    ret = ret && recFamilyIndEventa::CsvWriteTableFile( path );
+    ret = ret && recGallery::CsvWriteTableFile( path );
+    ret = ret && recGalleryMedia::CsvWriteTableFile( path );
+    ret = ret && recIndividualEvent::CsvWriteTableFile( path );
+    ret = ret && recIndividual::CsvWriteTableFile( path );
+    ret = ret && recIndividualPersona::CsvWriteTableFile( path );
+    ret = ret && recMedia::CsvWriteTableFile( path );
+    ret = ret && recName::CsvWriteTableFile( path );
+    ret = ret && recNamePart::CsvWriteTableFile( path );
+    ret = ret && recNamePartType::CsvWriteTableFile( path );
+    ret = ret && recNameStyle::CsvWriteTableFile( path );
+    ret = ret && recPersona::CsvWriteTableFile( path );
+    ret = ret && recPlace::CsvWriteTableFile( path );
+    ret = ret && recPlacePart::CsvWriteTableFile( path );
+    ret = ret && recPlacePartType::CsvWriteTableFile( path );
+    ret = ret && recReferenceEntity::CsvWriteTableFile( path );
+    ret = ret && recReference::CsvWriteTableFile( path );
+    ret = ret && recResearcher::CsvWriteTableFile( path );
+    ret = ret && recSystem::CsvWriteTableFile( path );
+    ret = ret && recUserSetting::CsvWriteTableFile( path );
+    ret = ret && recUser::CsvWriteTableFile( path );
     return ret;
 }
+
+
+// End of recCsv.cpp file
