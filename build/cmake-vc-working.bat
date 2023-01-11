7da8b30f7d41b5d9d0c720d5abfe54944c99a4e3
Nick Matthews <nick@kizzyco.com>
2023-01-11 17:54:07 +0000

    build/cmake-vc-working.bat creates the tfp project
    Creating the extended project is carried out outside the directory tree.

--- build/cmake-vc-working.bat
+++ build/cmake-vc-working.bat
@@ -2,6 +2,6 @@ rem Create or update vc-working CMake build sys
 
 if not exist vc-working md vc-working
 cd vc-working
-cmake -G "Visual Studio 17 2022" ../../..
+cmake -G "Visual Studio 17 2022" ../..
 
 pause
